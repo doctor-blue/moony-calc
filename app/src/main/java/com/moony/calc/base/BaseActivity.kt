fbe70c7d47c8dd2d626ced038c164c2d62aa9f55
Admin <dangtung789.td@gmail.com>
2020-02-09 09:58:59 +0700

    setup fist app include :'base activity, base fragment,icon '

--- app/src/main/java/com/moony/calc/base/BaseActivity.kt
+++ app/src/main/java/com/moony/calc/base/BaseActivity.kt
@@ -1,7 +1,19 @@
 package com.moony.calc.base
 
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.annotation.Nullable
 
-abstract class BaseActivity :AppCompatActivity() {
 
+abstract class BaseActivity : AppCompatActivity() {
+    abstract val layoutId: Int
+    override fun onCreate(@Nullable savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layoutId)
+
+        init(savedInstanceState)
+
+    }
+
+    abstract fun init(savedInstanceState: Bundle?)
 }
--- app/src/main/java/com/moony/calc/base/BaseFagment.kt
+++ app/src/main/java/com/moony/calc/base/BaseFagment.kt
@@ -1,6 +1,34 @@
 package com.moony.calc.base
 
+import android.os.Bundle
+import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
+import android.view.ViewGroup
+import android.view.LayoutInflater
+import androidx.annotation.NonNull
+import android.app.Activity
+import android.view.View
 
-abstract class BaseFagment:Fragment() {
+
+abstract class BaseFragment() : Fragment() {
+
+    var activity: Activity? = null
+
+    abstract val layoutId: Int
+    override fun onCreateView(
+
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+        var view:View? = inflater.inflate(layoutId, container, false)
+        activity = getActivity()
+        init()
+        return view
+    }
+
+    abstract fun init()
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
 }
