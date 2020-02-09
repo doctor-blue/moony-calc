27b630d9f59a701752c44ad7fd88be0872b2b1f7
Admin <dangtung789.td@gmail.com>
2020-02-09 12:40:53 +0700

    Tan setup

--- app/src/main/java/com/moony/calc/base/BaseActivity.kt
+++ app/src/main/java/com/moony/calc/base/BaseActivity.kt
@@ -3,17 +3,24 @@ package com.moony.calc.base
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.annotation.Nullable
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 
 abstract class BaseActivity : AppCompatActivity() {
-    abstract val layoutId: Int
+
     override fun onCreate(@Nullable savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(layoutId)
+        setContentView(getLayoutId())
 
         init(savedInstanceState)
 
     }
 
     abstract fun init(savedInstanceState: Bundle?)
+    abstract fun getLayoutId(): Int
 }
