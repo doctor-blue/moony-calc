8c050060b6f0ddd8751e36957315949287b02347
unknown <hn.minhvu@gmail.com>
2020-06-06 09:48:58 +0700

    pull code

--- app/src/main/java/com/moony/calc/adapter/SavingBoxAdapter.kt
+++ app/src/main/java/com/moony/calc/adapter/SavingBoxAdapter.kt
@@ -1,39 +1,51 @@
 package com.moony.calc.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.RecyclerView
 import com.daimajia.numberprogressbar.NumberProgressBar
 import com.google.android.material.textview.MaterialTextView
 import com.moony.calc.R
 import com.moony.calc.model.Saving
 
-class SavingBoxAdapter(private val context: Context, private val savings: List<Saving>, private val onClick: (Saving) -> Unit): RecyclerView.Adapter<SavingBoxAdapter.SavingViewHolder>() {
+class SavingBoxAdapter(
+    private val context: Context,
+    private val savings: List<Saving>
+//    private val onClick: (Unit) -> Unit
+) : RecyclerView.Adapter<SavingBoxAdapter.SavingViewHolder>() {
 
-    inner class SavingViewHolder(itemView: View, private val onClick: (Saving) -> Unit): RecyclerView.ViewHolder(itemView) {
-
+    inner class SavingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val imgGoalCategoryItem: ImageView = itemView.findViewById(R.id.img_goal_category_item)
-        val txtSavingDescriptionItem: MaterialTextView = itemView.findViewById(R.id.txt_saving_description_item)
+        val txtSavingDescriptionItem: MaterialTextView =
+            itemView.findViewById(R.id.txt_saving_description_item)
+        val txtAmountProgressItem: MaterialTextView =
+            itemView.findViewById(R.id.txt_amount_progress_item)
+        val pbSavingGoalsItem: NumberProgressBar = itemView.findViewById(R.id.pb_saving_goals_item)
 
+        fun onBind(saving:Saving){
+            imgGoalCategoryItem.setImageResource(saving.idCategory)
+            txtSavingDescriptionItem.text = saving.description
+            txtAmountProgressItem.text = saving.desiredAmount.toString()
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SavingViewHolder {
-        TODO("Not yet implemented")
+        val view: View =
+            LayoutInflater.from(context).inflate(R.layout.saving_goals_item, parent, false)
+        return SavingViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = savings.size
+
 
     override fun onBindViewHolder(holder: SavingViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.onBind(savings[position])
     }
-
-
-
 }
--- app/src/main/java/com/moony/calc/fragments/SavingBoxFragment.kt
+++ app/src/main/java/com/moony/calc/fragments/SavingBoxFragment.kt
@@ -1,12 +1,26 @@
 package com.moony.calc.fragments
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.moony.calc.R
 import com.moony.calc.activities.AddSavingGoalActivity
+import com.moony.calc.adapter.SavingBoxAdapter
 import com.moony.calc.base.BaseFragment
+import com.moony.calc.database.SavingViewModel
+import com.moony.calc.model.Saving
 import kotlinx.android.synthetic.main.fragment_saving_box.*
 
-class SavingBoxFragment : BaseFragment() {
+class SavingBoxFragment() : BaseFragment() {
+    private lateinit var savings: List<Saving>
+    private val savingViewModel: SavingViewModel by lazy { ViewModelProvider(this)[SavingViewModel::class.java] }
+//    private val onClick: (Any) -> Unit
+
+
     override fun init() {
         initControl()
         initEvent()
@@ -19,10 +33,17 @@ class SavingBoxFragment : BaseFragment() {
     }
 
     private fun initControl() {
+        rv_saving_box.layoutManager = LinearLayoutManager(context)
 
+        savingViewModel.getAllSaving().observe(this,
+            Observer<List<Saving>> { t ->
+                if (t != null) {
+                    savings = t
+                }
+            })
+        val adapter = SavingBoxAdapter(baseContext, savings)
     }
 
     override fun getLayoutId(): Int = R.layout.fragment_saving_box
-
 
 }
