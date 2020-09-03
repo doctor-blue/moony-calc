c3f320fcf136a3803b18b533e10eb25ad467bf4f
Nguyễn Hoàng Ân <an2419998@gmail.com>
2020-09-03 19:36:23 +0700

    fixing UI for imageView, switch, textSize, font, spinner

--- app/src/main/java/com/moony/calc/fragments/SettingFragment.kt
+++ app/src/main/java/com/moony/calc/fragments/SettingFragment.kt
@@ -1,11 +1,29 @@
 package com.moony.calc.fragments
 
+import android.widget.ArrayAdapter
 import com.moony.calc.R
 import com.moony.calc.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_setting.*
 
 class SettingFragment : BaseFragment() {
     override fun init() {
+        initEvent()
+        initControl()
+    }
 
+    private fun initEvent() {
+    }
+
+    private fun initControl() {
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.Currency,
+            R.layout.spinner_item
+        ).also { arrayAdapter ->
+            arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            settings_spinner.adapter = arrayAdapter
+
+        }
     }
 
     override fun getLayoutId(): Int = R.layout.fragment_setting
