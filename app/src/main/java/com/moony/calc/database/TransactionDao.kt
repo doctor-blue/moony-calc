daa0d5ebd363d956580d519efb9a9d20aa9ee247
vuhm2012 <hn.minhvu@gmail.com>
2021-03-19 11:43:10 +0700

    Update ChartFragment.kt

--- app/src/main/java/com/moony/calc/database/TransactionDao.kt
+++ app/src/main/java/com/moony/calc/database/TransactionDao.kt
@@ -2,6 +2,7 @@ package com.moony.calc.database
 
 import androidx.lifecycle.LiveData
 import androidx.room.*
+import com.moony.calc.model.ChartItem
 import com.moony.calc.model.Transaction
 import com.moony.calc.model.TransactionItem
 
@@ -29,7 +30,9 @@ interface TransactionDao {
     @Query("delete from transaction_table where idCategory=:idCategory")
     suspend fun deleteAllTransactionByCategory(idCategory: Int)
 
-    @Query("select transaction_table.*, category_table.* from transaction_table inner join category_table on transaction_table.idCategory = category_table.idCategory where month=:month and year=:year ")
+    @Query("select transaction_table.*, category_table.* from transaction_table inner join category_table on transaction_table.idCategory = category_table.idCategory where month=:month and year=:year")
     fun getAllTransactionItem(month: Int, year: Int): LiveData<List<TransactionItem>>
 
+    @Query("select transaction_table.*, category_table.*, sum(transaction_table.money) as 'sum' from transaction_table inner join category_table on transaction_table.idCategory = category_table.idCategory where month=:month and year=:year group by category_table.idCategory")
+    fun getChartItem(month: Int, year: Int): LiveData<List<ChartItem>>
 }
--- app/src/main/java/com/moony/calc/ui/chart/ChartFragment.kt
+++ app/src/main/java/com/moony/calc/ui/chart/ChartFragment.kt
@@ -1,13 +1,59 @@
 package com.moony.calc.ui.chart
 
+import android.util.Log
+import androidx.annotation.ColorRes
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.faskn.lib.PieChart
+import com.faskn.lib.Slice
 import com.moony.calc.R
 import com.moony.calc.base.BaseFragment
+import com.moony.calc.model.ChartItem
+import com.moony.calc.ui.transaction.TransactionViewModel
 import kotlinx.android.synthetic.main.fragment_chart.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class ChartFragment : BaseFragment() {
-    override fun init() {
 
+    var slices = arrayListOf<Slice>()
+    var sum: Double = 0.0
+
+    @ColorRes val colors = arrayListOf<Int>(R.color.colorAccent, R.color.blue, R.color.colorPrimary, R.color.backgroundColor, R.color.brown_tint)
+
+    private val transactionViewModel: TransactionViewModel by lazy {
+        ViewModelProvider(fragmentActivity!!)[TransactionViewModel::class.java]
+    }
+
+    override fun init() {
+        initControl()
+        initEvent()
     }
 
     override fun getLayoutId(): Int = R.layout.fragment_chart
+
+    private fun initControl() {
+        generateData()
+    }
+
+    private fun initEvent() {
+
+    }
+
+    private fun generateData() {
+        transactionViewModel.getChartData(2, 2021).observe(viewLifecycleOwner, Observer {
+            for((i, item) in it.withIndex()) {
+                slices.add(Slice(item.sum.toFloat(), colors[i], item.category.title))
+                Log.d("DATA", i.toString())
+            }
+
+            val pieChart = PieChart(
+                slices = slices, clickListener = null, sliceStartPoint = 0f, sliceWidth = 80f
+            ).build()
+
+            chart.setPieChart(pieChart)
+
+        })
+    }
 }
