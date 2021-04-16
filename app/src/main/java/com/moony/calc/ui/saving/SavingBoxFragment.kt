041ca4b72ebf281a4e9131cc520045e18a57e33c
Nguyen Van Tan <Doctorblue.king@gmail.com>
2021-04-16 08:15:09 +0700

    Fix goal count

--- app/src/main/java/com/moony/calc/ui/saving/SavingBoxFragment.kt
+++ app/src/main/java/com/moony/calc/ui/saving/SavingBoxFragment.kt
@@ -14,7 +14,14 @@ import com.moony.calc.model.Saving
 class SavingBoxFragment : BaseFragment() {
     private lateinit var savings: List<Saving>
     private val savingViewModel: SavingViewModel by lazy { ViewModelProvider(this)[SavingViewModel::class.java] }
-    lateinit var adapter: SavingBoxAdapter
+    private val adapter: SavingBoxAdapter by lazy {
+        SavingBoxAdapter(requireActivity(), countCompletedGoals) {
+            val saving: Saving = it as Saving
+            val intent = Intent(baseContext, SavingDetailActivity::class.java)
+            intent.putExtra(SAVING_KEY, saving)
+            startActivity(intent)
+        }
+    }
 
     companion object {
         const val SAVING_KEY = "com.moony.calc.ui.saving.SavingBoxFragment.SAVING_KEY"
@@ -28,33 +35,30 @@ class SavingBoxFragment : BaseFragment() {
         binding.btnAddSavingGoals.setOnClickListener {
             findNavController().navigate(R.id.action_saving_fragment_to_addSavingGoalActivity)
         }
-        adapter = SavingBoxAdapter(requireActivity(), countCompletedGoals) {
-            val saving: Saving = it as Saving
-            val intent = Intent(baseContext, SavingDetailActivity::class.java)
-            intent.putExtra(SAVING_KEY, saving)
-            startActivity(intent)
-        }
     }
 
     override fun initControls(view: View, savedInstanceState: Bundle?) {
         binding.rvSavingBox.layoutManager =
             LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        binding.rvSavingBox.adapter = adapter
+
         savingViewModel.getAllSaving().observe(this,
             { savings ->
                 this.savings = savings
                 adapter.setSavings(savings)
+
                 if (savings.isNotEmpty()) {
-                    binding.rvSavingBox.adapter = adapter
                     binding.layoutListEmpty.visibility = View.GONE
                     binding.rvSavingBox.visibility = View.VISIBLE
-
-                    binding.txtTotalGoal.text = "${savings.size}"
-                    binding.txtTotalCompleted.text = "$goalsCompleted"
-                    binding.txtTotalInProgress.text = "${savings.size - goalsCompleted}"
                 } else {
                     binding.layoutListEmpty.visibility = View.VISIBLE
                     binding.rvSavingBox.visibility = View.GONE
+                    goalsCompleted = 0
                 }
+
+                binding.txtTotalGoal.text = "${savings.size}"
+                binding.txtTotalCompleted.text = "$goalsCompleted"
+                binding.txtTotalInProgress.text = "${savings.size - goalsCompleted}"
 
             })
     }
