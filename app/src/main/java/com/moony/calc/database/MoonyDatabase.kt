3a19bc1a36b0fed0bf0eb7ac2f567f9cf968a08c
Nguyen Van Tan <Doctorblue.king@gmail.com>
2021-04-18 15:23:36 +0700

    Pull origin fixing

--- app/src/main/java/com/moony/calc/database/MoonyDatabase.kt
+++ app/src/main/java/com/moony/calc/database/MoonyDatabase.kt
@@ -39,16 +39,15 @@ abstract class MoonyDatabase : RoomDatabase() {
         private val callback = object : RoomDatabase.Callback() {
             override fun onCreate(db: SupportSQLiteDatabase) {
                 super.onCreate(db)
-                adDefaultCategory(instance!!.getCategoryDao())
+//                adDefaultCategory(instance!!.getCategoryDao())
             }
         }
 
-        private fun adDefaultCategory(categoryDao: CategoryDao) {
-            GlobalScope.launch(Dispatchers.IO) {
-                categoryDao.insertCategory(Category("","categories/income/salary.png",resId = R.string.saving))
-                categoryDao.insertCategory(Category("","categories/income/salary.png",true,resId = R.string.saving))
-            }
-        }
+//        private fun adDefaultCategory(categoryDao: CategoryDao) {
+//            GlobalScope.launch(Dispatchers.IO) {
+//            }
+//        }
+
     }
 
 }
