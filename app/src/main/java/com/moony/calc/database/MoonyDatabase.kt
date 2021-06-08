4b98fe742548a168b22b40f8b9d126a69a438d6e
quangkhaitran <khaiquang2800@gmail.com>
2021-06-08 16:02:22 +0700

    pull origin testing

--- app/src/main/java/com/moony/calc/database/MoonyDatabase.kt
+++ app/src/main/java/com/moony/calc/database/MoonyDatabase.kt
@@ -50,7 +50,34 @@ abstract class MoonyDatabase : RoomDatabase() {
         private fun adDefaultCategory(categoryDao: CategoryDao) {
             GlobalScope.launch(Dispatchers.IO) {
                 categoryDao.insertCategory(Category("","categories/income/salary.png",resId = R.string.saving))
+                categoryDao.insertCategory(Category("","categories/education/book.png",resId = R.string.books))
+                categoryDao.insertCategory(Category("","categories/electronics/phone.png",resId = R.string.phones))
+                categoryDao.insertCategory(Category("","categories/entertainment/gamepad.png",resId = R.string.games))
+                categoryDao.insertCategory(Category("","categories/entertainment/cinema.png",resId = R.string.cinema))
+                categoryDao.insertCategory(Category("","categories/family/baby.png",resId = R.string.baby))
+                categoryDao.insertCategory(Category("","categories/family/cat.png",resId = R.string.pet))
+                categoryDao.insertCategory(Category("","categories/fitness/workout.png",resId = R.string.fitnesss))
+                categoryDao.insertCategory(Category("","categories/food/burger.png",resId = R.string.foods))
+                categoryDao.insertCategory(Category("","categories/food/juice.png",resId = R.string.drinks))
+                categoryDao.insertCategory(Category("","categories/food/french_fries.png",resId = R.string.snacks))
+                categoryDao.insertCategory(Category("","categories/furniture/wrench.png",resId = R.string.repair))
+                categoryDao.insertCategory(Category("","categories/life/travel.png",resId = R.string.travel))
+                categoryDao.insertCategory(Category("","categories/life/heart.png",resId = R.string.lover))
+                categoryDao.insertCategory(Category("","categories/medical/medicine.png",resId = R.string.medicine))
+                categoryDao.insertCategory(Category("","categories/medical/vacine.png",resId = R.string.health_check))
+                categoryDao.insertCategory(Category("","categories/shopping/shoppingonline.png",resId = R.string.shoppingg))
+                categoryDao.insertCategory(Category("","categories/transportation/station.png",resId = R.string.bus_ticket))
+                categoryDao.insertCategory(Category("","categories/transportation/train.png",resId = R.string.train_ticket))
+                categoryDao.insertCategory(Category("","categories/transportation/plane.png",resId = R.string.plane_ticket))
+                categoryDao.insertCategory(Category("","categories/transportation/taxi.png",resId = R.string.taxi))
+                categoryDao.insertCategory(Category("","categories/transportation/parking_lot.png",resId = R.string.parking))
+                categoryDao.insertCategory(Category("","categories/transportation/fuel.png",resId = R.string.refuel))
+                categoryDao.insertCategory(Category("","categories/money/pay.png",resId = R.string.loan))
+                categoryDao.insertCategory(Category("","categories/money/money.png",resId = R.string.other))
+
+
                 categoryDao.insertCategory(Category("","categories/income/salary.png",true,resId = R.string.saving))
+
             }
         }
 
