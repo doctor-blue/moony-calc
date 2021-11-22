db1b30a45992cc9e4ed0e0407c12d3959f03cb13
Nguyen Van Tan <doctorblue.king@gmail.com>
2021-11-22 15:06:02 +0700

    Update default category

--- app/src/main/java/com/moony/calc/database/MoonyDatabase.kt
+++ app/src/main/java/com/moony/calc/database/MoonyDatabase.kt
@@ -1,5 +1,6 @@
 package com.moony.calc.database
 
+import android.annotation.SuppressLint
 import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
@@ -26,8 +27,10 @@ abstract class MoonyDatabase : RoomDatabase() {
     abstract fun getCategoryDao(): CategoryDao
     abstract fun getSavingDao(): SavingDao
     abstract fun getSavingHistoryDao(): SavingHistoryDao
+    lateinit var context: Context
 
     companion object {
+        @SuppressLint("StaticFieldLeak")
         @Volatile
         private var instance: MoonyDatabase? = null
         fun getInstance(context: Context): MoonyDatabase {
@@ -38,6 +41,7 @@ abstract class MoonyDatabase : RoomDatabase() {
                         MoonyDatabase::class.java,
                         "MoonyDatabase.db"
                     ).addCallback(callback).build()
+                    instance!!.context = context
                 }
             return instance!!
         }
@@ -45,55 +49,78 @@ abstract class MoonyDatabase : RoomDatabase() {
         private val callback = object : RoomDatabase.Callback() {
             override fun onCreate(db: SupportSQLiteDatabase) {
                 super.onCreate(db)
-                addDefaultCategory(instance!!.getCategoryDao())
+                addDefaultCategory(instance!!.getCategoryDao(), instance!!.context)
             }
         }
 
-        private fun addDefaultCategory(categoryDao: CategoryDao) {
+        private fun addDefaultCategory(categoryDao: CategoryDao,context: Context) {
             GlobalScope.launch(Dispatchers.IO) {
                 val defaultCategories = listOf(
-                    Category("","categories/life/atm.png",resId = R.string.atm),
-                    Category("","categories/education/book.png",resId = R.string.books),
-                    Category("","categories/electronics/phone.png",resId = R.string.phones),
-                    Category("","categories/entertainment/gamepad.png",resId = R.string.games),
-                    Category("","categories/entertainment/cinema.png",resId = R.string.cinema),
-                    Category("","categories/family/baby.png",resId = R.string.baby),
-                    Category("","categories/family/cat.png",resId = R.string.pet),
-                    Category("","categories/fitness/workout.png",resId = R.string.fitnesss),
-                    Category("","categories/food/burger.png",resId = R.string.foods),
-                    Category("","categories/food/juice.png",resId = R.string.drinks),
-                    Category("","categories/food/french_fries.png",resId = R.string.snacks),
-                    Category("","categories/furniture/wrench.png",resId = R.string.repair),
-                    Category("","categories/life/travel.png",resId = R.string.travel),
-                    Category("","categories/life/heart.png",resId = R.string.lover),
-                    Category("","categories/life/baohiem.png",resId = R.string.insurrance),
-                    Category("","categories/life/bill.png",resId = R.string.bill),
-                    Category("","categories/life/rent.png",resId = R.string.rent),
-                    Category("","categories/life/wedding.png",resId = R.string.wedding),
-                    Category("","categories/life/charity.png",resId = R.string.charity),
-                    Category("","categories/life/party.png",resId = R.string.party),
-                    Category("","categories/medical/medicine.png",resId = R.string.medicine),
-                    Category("","categories/medical/vacine.png",resId = R.string.health_check),
-                    Category("","categories/shopping/shoppingonline.png",resId = R.string.shoppingg),
-                    Category("","categories/shopping/clothes.png",resId = R.string.clothes),
-                    Category("","categories/shopping/footwear.png",resId = R.string.footwear),
-                    Category("","categories/transportation/station.png",resId = R.string.bus_ticket),
-                    Category("","categories/transportation/train.png",resId = R.string.train_ticket),
-                    Category("","categories/transportation/plane.png",resId = R.string.plane_ticket),
-                    Category("","categories/transportation/taxi.png",resId = R.string.taxi),
-                    Category("","categories/transportation/parking_lot.png",resId = R.string.parking),
-                    Category("","categories/transportation/fuel.png",resId = R.string.refuel),
-                    Category("","categories/income/salary.png",resId = R.string.saving),
-                    Category("","categories/money/money.png",resId = R.string.other),
+                    Category(context.getString(R.string.atm), "categories/life/atm.png"),
+                    Category(context.getString(R.string.books), "categories/education/book.png"),
+                    Category(context.getString(R.string.phones), "categories/electronics/phone.png"),
+                    Category(context.getString(R.string.games), "categories/entertainment/gamepad.png"),
+                    Category(context.getString(R.string.cinema), "categories/entertainment/cinema.png"),
+                    Category(context.getString(R.string.baby), "categories/family/baby.png"),
+                    Category(context.getString(R.string.pet), "categories/family/cat.png"),
+                    Category(context.getString(R.string.fitness), "categories/fitness/workout.png"),
+                    Category(context.getString(R.string.foods), "categories/food/burger.png"),
+                    Category(context.getString(R.string.drinks), "categories/food/juice.png"),
+                    Category(context.getString(R.string.snacks), "categories/food/french_fries.png"),
+                    Category(context.getString(R.string.repair), "categories/furniture/wrench.png"),
+                    Category(context.getString(R.string.travel), "categories/life/travel.png"),
+                    Category(context.getString(R.string.lover), "categories/life/heart.png"),
+                    Category(context.getString(R.string.insurrance), "categories/life/baohiem.png"),
+                    Category(context.getString(R.string.bill), "categories/life/bill.png"),
+                    Category(context.getString(R.string.rent), "categories/life/rent.png"),
+                    Category(context.getString(R.string.wedding), "categories/life/wedding.png"),
+                    Category(context.getString(R.string.charity), "categories/life/charity.png"),
+                    Category(context.getString(R.string.party), "categories/life/party.png"),
+                    Category(context.getString(R.string.medicine), "categories/medical/medicine.png"),
+                    Category(context.getString(R.string.health_check), "categories/medical/vacine.png"),
+                    Category(
+                        context.getString(R.string.shopping),
+                        "categories/shopping/shoppingonline.png"
+                    ),
+                    Category(context.getString(R.string.clothes), "categories/shopping/clothes.png"),
+                    Category(context.getString(R.string.footwear), "categories/shopping/footwear.png"),
+                    Category(
+                        context.getString(R.string.bus_ticket),
+                        "categories/transportation/station.png",
+                    ),
+                    Category(
+                        context.getString(R.string.train_ticket),
+                        "categories/transportation/train.png",
+                    ),
+                    Category(
+                        context.getString(R.string.plane_ticket),
+                        "categories/transportation/plane.png",
+                    ),
+                    Category(context.getString(R.string.taxi), "categories/transportation/taxi.png"),
+                    Category(
+                        context.getString(R.string.parking),
+                        "categories/transportation/parking_lot.png",
+                    ),
+                    Category(context.getString(R.string.refuel), "categories/transportation/fuel.png"),
+                    Category(context.getString(R.string.saving), "categories/income/salary.png"),
+                    Category(context.getString(R.string.other), "categories/money/money.png"),
 
-                    Category("","categories/income/sell.png",true,resId = R.string.sell),
-                    Category("","categories/income/interest.png",true,resId = R.string.bank_interest),
-                    Category("","categories/income/bonus.png",true,resId = R.string.bonus),
-                    Category("","categories/income/salary1.png",true,resId = R.string.monthly_salary),
-                    Category("","categories/income/salary.png",true,resId = R.string.saving),
-                    Category("","categories/money/money.png",true,resId = R.string.other)
+                    Category(context.getString(R.string.sell), "categories/income/sell.png", true),
+                    Category(
+                        context.getString(R.string.bank_interest),
+                        "categories/income/interest.png",
+                        true,
+                    ),
+                    Category(context.getString(R.string.bonus), "categories/income/bonus.png", true),
+                    Category(
+                        context.getString(R.string.monthly_salary),
+                        "categories/income/salary1.png",
+                        true,
+                    ),
+                    Category(context.getString(R.string.saving), "categories/income/salary.png", true),
+                    Category(context.getString(R.string.other), "categories/money/money.png", true)
                 )
-                defaultCategories.forEach{
+                defaultCategories.forEach {
                     categoryDao.insertCategory(it)
                 }
             }
